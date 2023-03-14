a9cdb2eb6718191ad8e2e4dda5a3b3d41927bb0a
MarioBlasi <mblasi509@gmail.com>
2023-03-14 14:55:01 +0000

    buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,6 +36,18 @@
 // eventListener
 // increment/descrement
 
+// selezioniamo l'elemento del DOM che conterrà le immagini
+const carouselContainer = document.querySelector(".carousel-container");
+
+// definiamo l'array di immagini
+const images = [
+  "img/01.webp",
+  "img/02.webp",
+  "img/03.webp",
+  "img/04.webp",
+  "img/05.webp",
+];
+
 // passiamo sulle immagini e aggiungiamo un nuovo elemento alla DOM utilizzando for
 for (let i = 0; i < images.length; i++) {
   const imgSrc = images[i];
@@ -43,3 +55,55 @@ for (let i = 0; i < images.length; i++) {
   //innerHTML
   imagesElement.innerHTML += imgElement;
 }
+
+//next button
+const nextEl = document.querySelector(".next");
+nextEl.addEventListener("click", function () {
+  console.log("cliccato next");
+
+  //select all slides
+  const slideImageElements = document.querySelectorAll(
+    ".slider > .images > img"
+  );
+  console.log(slideImageElements); // array [index]
+  // select the current slide
+
+  const currentSlide = slideImageElements[activeImage];
+  console.log(currentSlide);
+
+  currentSlide.classList.remove("active");
+
+  activeImage++;
+
+  console.log(activeImage);
+  const nextImage = slideImagesElements[activeImage];
+
+  console.log(nextImage);
+  nextImage.classList.add("active");
+});
+
+//prev button
+const prevEl = document.querySelector(".prev");
+prevEl.addEventListener("click", function () {
+  console.log("cliccato prev");
+
+  //select all slides
+  const slideImageElements = document.querySelectorAll(
+    ".slider > .images > img"
+  );
+  console.log(slideImageElements); // array [index]
+  // select the current slide
+
+  const currentSlide = slideImageElements[activeImage];
+  console.log(currentSlide);
+
+  currentSlide.classList.remove("active");
+
+  activeImage--;
+
+  console.log(activeImage);
+  const prevImage = slideImagesElements[activeImage];
+
+  console.log(prevImage);
+  prevImage.classList.add("active");
+});
